a4dd880b63cc250f1fc2f9dc8e24d28feee9337c
andreapmacedo <aa.andremacedo@gmail.com>
2022-11-29 23:19:39 -0300

    feat: Adiciona sidebar.

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,9 @@
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+  return (
+    <sidebar className={styles.sidebar}>
+      <strong>sidebar</strong>
+    </sidebar>
+  );
+}
